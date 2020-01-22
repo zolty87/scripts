d6aeba2b435dcdbc7393c05f43cac60fbedbb1db
Przemyslaw Zoltowski <Przemyslaw.Zoltowski@adm.uw.edu.pl>
2020-01-22 16:52:38 +0100

    zmieniam

--- update_dns.sh
+++ update_dns.sh
@@ -5,4 +5,4 @@
 
 dns_file=~/.dns_completion
 
-(host -l net.uw.edu.pl 193.0.71.133 && host -l rtr.net.uw.edu.pl 193.0.71.133 && (cat ~/.ssh/known_hosts | cut -f 1 -d " "|tr -s "," "\n"|sort|uniq) )| grep -v "name server" | awk '{print$1}' | sort | uniq | grep -v "Address:\|Aliases:\|Name:\|Using" > $dns_file
+(host -l net.uw.edu.pl ns3.uw.edu.pl && host -l rtr.net.uw.edu.pl ns3.uw.edu.pl && (cat ~/.ssh/known_hosts | cut -f 1 -d " "|tr -s "," "\n"|sort|uniq) )| grep -v "name server" | awk '{print$1}' | sort | uniq | grep -v "Address:\|Aliases:\|Name:\|Using" > $dns_file
